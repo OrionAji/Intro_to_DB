16056efc9d997d8b7318228975ac07ed003c893c
Gamaliel Aji <132375835+OrionAji@users.noreply.github.com>
2026-01-19 15:34:24 +0100

    Minor bug-fix with spacing in some customer data input

--- task_6.sql
+++ task_6.sql
@@ -21,14 +21,14 @@ INSERT INTO customer (
      3,
      'Obed Ehoneah',
      'eobed@sandtech.com',
-     '125 Happiness Ave.'
+     '125 Happiness  Ave.'
     )
 
     (
      4,
      'Nehemial Kamolu',
      'nkamolu@sandtech.com',
-     '126 Happiness Ave.'
+     '126 Happiness  Ave.'
     )
 );
 
