2806396c9d8820605f3eebf0ee3d05f4186bd873
Gamaliel Aji <132375835+OrionAji@users.noreply.github.com>
2026-01-19 15:24:36 +0100

    Added a single row of data to my table

---
+++ task_5.sql
@@ -0,0 +1,11 @@
+USE alx_book_store;
+
+INSERT INTO Customer_id VALUES (1);
+INSERT INTO customer_name VALUES ('Cole Baidoo');
+INSERT INTO email VALUES ('cbaidoo@sandtech.com');
+INSERT INTO address VALUES ('123 Happiness Ave.');
+
+);
+
+cursor.execute(sql_query)
+cursor.close()
